2d883d2c1f2ca23b3039f481fe3afc60b39ff5ea
FranciscoDarlan <darlann_c_jpa@hotmail.com>
2024-02-08 17:15:34 -0300

    desafio1 alura

---
+++ scripts.js
@@ -0,0 +1,29 @@
+alert('Boas Vindas ao Nosso Site!')
+
+let nome = 'Lua';
+
+
+let idade = 25;
+
+
+let numeroDeVendas = 50;
+
+
+
+let saldoDisponivel = 1000;
+
+
+alert('alerta de Erro! Preencha todos os campos');
+
+let mensagemDeErro = 'Erro com let! Preencha todos os campos';
+alert(mensagemDeErro);
+
+let usuario = prompt('Qual Nome do Usuário?');
+console.log(usuario)
+
+let idade_P = prompt('Qual sua Idade?');
+console.log(idade_P)
+
+
+
+
